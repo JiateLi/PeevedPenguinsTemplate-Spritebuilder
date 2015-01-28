c4626d03087c344ceb1f5f2d65c359cf9d29d8a7
JiateLi <lijiate1992@gmail.com>
2015-01-27 20:37:34 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Jiate Li on 15/1/27.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
